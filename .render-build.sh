c82bb894237e4a317cfa1e57788c782ef119c077
HumbertoCastroHorizonti <humberto.castro@horizonti.digital>
2024-05-08 19:46:52 -0300

    other test

--- .render-build.sh
+++ .render-build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-# Ensure the required PHP version is installed
+# Install required packages
 apt-get update -y
-apt-get install -y php-cli php-mbstring php-xml unzip curl
+apt-get install -y software-properties-common curl unzip
+
+# Add the PHP repository
+add-apt-repository ppa:ondrej/php -y
+apt-get update -y
+
+# Install PHP and necessary extensions
+apt-get install -y php8.0 php8.0-cli php8.0-mbstring php8.0-xml php8.0-zip php8.0-curl
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
